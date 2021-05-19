bc3271c9431a3bd2ff1e8f539dd028bab122e356
Crami <cramitus@gmail.com>
2021-05-19 21:30:31 +0200

    Mejoras en el HUD

--- Elemental/Assets/Scripts/LavaController.cs
+++ Elemental/Assets/Scripts/LavaController.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class LavaController : MonoBehaviour
 {
     private Vector3 MinScale = new Vector3(0, 0, 0);
-    private float WaitTime = 50;
+    private float WaitTime = 6000;
     private float WaitedTime;
 
     // Start is called before the first frame update
@@ -20,14 +20,19 @@ public class LavaController : MonoBehaviour
         float delta = Time.deltaTime * 1000;
         WaitedTime += delta;
 
-        if (WaitedTime >= WaitTime && MinScale.x <= transform.localScale.x && MinScale.y <= transform.localScale.y)
-        {
-            transform.localScale = new Vector3(transform.localScale.x - 0.005f, transform.localScale.y - 0.005f, transform.localScale.z );
-            WaitedTime = 0;
+        /* if (WaitedTime >= WaitTime && MinScale.x <= transform.localScale.x && MinScale.y <= transform.localScale.y)
+         {
+             transform.localScale = new Vector3(transform.localScale.x - 0.005f, transform.localScale.y - 0.005f, transform.localScale.z );
+             WaitedTime = 0;
 
-        }
+         }
 
-        if (transform.localScale.x < MinScale.x && transform.localScale.y < MinScale.y)
+         if (transform.localScale.x < MinScale.x && transform.localScale.y < MinScale.y)
+         {
+             Destroy(gameObject);
+         }
+        */
+        if (WaitedTime >= WaitTime)
         {
             Destroy(gameObject);
         }
