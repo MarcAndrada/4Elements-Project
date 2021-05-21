c9eb3947744668cf209f6f040aad113b95220e06
Daniel Moncayo <rcmoncayodaniel@gmail.com>
2021-05-21 18:25:03 +0200

    Animando

--- Elemental/Assets/Scripts/PlayerController.cs
+++ Elemental/Assets/Scripts/PlayerController.cs
@@ -6,14 +6,20 @@ public class PlayerController : MonoBehaviour
 {
     /*Referencias*/
     private Rigidbody2D rb2d;
-
+    private Animator animator;
     /*Atributos*/
     float speed;
     float life;
-
+    /*Animaciones*/
+    private int upId;
+    private int downId;
+    private int leftId;
+    private int rightId;
+    private int idleId;
     private void Awake()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Start is called before the first frame update
@@ -22,12 +28,62 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         speed = 5;
-        life = 5;
+        life = 6;
+        upId = Animator.StringToHash("Up");
+        downId = Animator.StringToHash("Down");
+        leftId = Animator.StringToHash("Left");
+        rightId = Animator.StringToHash("Right");
+        idleId = Animator.StringToHash("Idle");
+
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        if (rb2d.velocity == Vector2.zero)
+        {
+            animator.SetBool(idleId, true);
+            animator.SetBool(leftId, false);
+            animator.SetBool(rightId, false);
+            animator.SetBool(upId, false);
+            animator.SetBool(downId, false);
+        }
+        else if (rb2d.velocity.x > 0 && rb2d.velocity.y == 0)
+        {
+            animator.SetBool(idleId, false);
+            animator.SetBool(leftId, false);
+            animator.SetBool(rightId, true);
+            animator.SetBool(upId, false);
+            animator.SetBool(downId, false);
+
+        }
+        else if (rb2d.velocity.x > 0 && rb2d.velocity.y > 0)
+        {
+            animator.SetBool(idleId, false);
+            animator.SetBool(leftId, false);
+            animator.SetBool(rightId, false);
+            animator.SetBool(upId, true);
+            animator.SetBool(downId, false);
+        }
+        else if (rb2d.velocity.x < 0 && rb2d.velocity.y < 0)
+        {
+            animator.SetBool(idleId, false);
+            animator.SetBool(leftId, false);
+            animator.SetBool(rightId, false);
+            animator.SetBool(upId, false);
+            animator.SetBool(downId, true);
+        }
+        else if (rb2d.velocity.x < 0 && rb2d.velocity.y == 0)
+        {
+            animator.SetBool(idleId, false);
+            animator.SetBool(leftId, true);
+            animator.SetBool(rightId, false);
+            animator.SetBool(upId, false);
+            animator.SetBool(downId, false);
+        }
+
+
         float delta = Time.deltaTime*1000;
         if(PlayerInput.Vertical > 0)
         {
